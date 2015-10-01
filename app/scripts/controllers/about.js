79c45fbb75618e4db6be95b05a293ed5eba62e44
Tomo Masakura <tomo.masakura@gmail.com>
2015-10-01 15:50:41 +0900

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angular3App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angular3App
+ */
+angular.module('angular3App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angular3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angular3App
+ */
+angular.module('angular3App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
