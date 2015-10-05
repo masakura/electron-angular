2ae3c25315e70612f9e62c629564c69db5a43333
Tomo Masakura <tomo.masakura@gmail.com>
2015-10-05 13:31:36 +0900

    それっぽい画面になるように

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,59 @@
  * Controller of the angular3App
  */
 angular.module('angular3App')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($timeout) {
+    var vm = this;
+
+    var Inspection = function (title) {
+      this.title = title;
+      this.status = 'pending';
+      this.resultStatus = '';
+    };
+
+    Inspection.prototype.run = function () {
+      this.status = 'progress';
+      this.resultStatus = '';
+    };
+
+    Inspection.prototype.finish = function (resultStatus, result) {
+      this.status = 'finish';
+      this.resultStatus = resultStatus;
+      this.result = result;
+    };
+
+    var inspections = vm.inspections = [
+      new Inspection('Node.js のバージョンは?'),
+      new Inspection('npm のバージョンは?')
     ];
+
+    vm.inspect = function () {
+      inspections[0].run();
+
+      $timeout(function () {
+        inspections[0].finish('success', 'v4.1.1');
+
+        inspections[1].run();
+
+        $timeout(function () {
+          inspections[1].finish('error', 'Unknown');
+        }, 1500);
+      }, 1000);
+    }
+    vm.getStatusClass = function (inspection) {
+      return {
+        'list-group-item-info': inspection.status === 'progress',
+        'list-group-item-success': inspection.resultStatus === 'success',
+        'list-group-item-danger': inspection.resultStatus === 'error',
+        'list-group-item-warning': inspection.resultStatus === 'warning'
+      };
+    };
+    vm.getGlphicon = function (inspection) {
+      return {
+        'glyphicon-stop': inspection.status === 'pending',
+        'glyphicon-refresh': inspection.status === 'progress',
+        'glyphicon-ok-sign': inspection.resultStatus === 'success',
+        'glyphicon-exclamation-sign': inspection.resultStatus === 'error',
+        'glyphicon-info-sign': inspection.resultStatus === 'warning'
+      };
+    };
   });
